e5ce54717856474b4c2070dff6067cf969d7dcb6
trompete11 <lucas.hoettler@gmail.com>
2024-01-03 23:14:47 +0100

    World2 Level2 First draft
    first try to make level 2 in world 2

---
+++ Assets/Scripts/ObjectController/DoorController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [Tooltip("If it is false door can't be used")]
+    public bool Locked = false;
+    [Tooltip("It is true for remote control only")]
+    public bool Remote = false;
+    [Space]
+    [Tooltip("Door can be opened")]
+    public bool CanOpen = true;
+    [Tooltip("Door can be closed")]
+    public bool CanClose = true;
+    [Space]
+    [Tooltip("Door locked by red key (use key script to declarate any object as key)")]
+    public bool RedLocked = false;
+    public bool BlueLocked = false;
+    [Space]
+    public bool isOpened = false;
+    [Range(0f, 4f)]
+    [Tooltip("Speed for door opening, degrees per sec")]
+    public float OpenSpeed = 3f;
+
+    // NearView()
+    float distance;
+    float angleView;
+    Vector3 direction;
+
+    // Hinge
+    [HideInInspector]
+    public Rigidbody rbDoor;
+    HingeJoint hinge;
+    JointLimits hingeLim;
+    float currentLim;
+
+    public GameObject doorKey;
+
+    void Start()
+    {
+        rbDoor = GetComponent<Rigidbody>();
+        hinge = GetComponent<HingeJoint>();
+    }
+
+    void Update()
+    {
+
+        
+    }
+
+    public void Action() // void to open/close door
+    {
+        if (!Locked)
+        {
+            // key lock checking
+            if (!doorKey.activeInHierarchy)
+            {
+                Locked = false;
+            }
+            
+            // opening/closing
+            if (isOpened && CanClose && !RedLocked && !BlueLocked)
+            {
+                isOpened = false;
+            }
+            else if (!isOpened && CanOpen && !RedLocked && !BlueLocked)
+            {
+                isOpened = true;
+                rbDoor.AddRelativeTorque(new Vector3(0, 0, 20f)); 
+            }
+        
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // key lock checking
+            if (!doorKey.activeInHierarchy)
+            {
+                isOpened = true;
+                rbDoor.AddRelativeTorque(new Vector3(0, 0, 20f));
+            }
+        }
+    }
+
+    bool NearView() // it is true if you near interactive object
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        direction = transform.position - Camera.main.transform.position;
+        angleView = Vector3.Angle(Camera.main.transform.forward, direction);
+        if (distance < 3f) return true; // angleView < 35f && 
+        else return false;
+    }
+
+    private void FixedUpdate() // door is physical object
+    {
+        if (isOpened)
+        {
+            currentLim = 85f;
+        }
+        else
+        {
+            // currentLim = hinge.angle; // door will closed from current opened angle
+            if (currentLim > 1f)
+                currentLim -= .5f * OpenSpeed;
+        }
+
+        // using values to door object
+        hingeLim.max = currentLim;
+        hingeLim.min = -currentLim;
+        hinge.limits = hingeLim;
+    }
+}
---
+++ Assets/Scripts/ObjectController/DoorKeyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class DoorKeyController : MonoBehaviour
+{
+    [Tooltip("True - red key object, false - blue key")]
+    public bool isRedKey = true;
+    AN_HeroInteractive hero;
+
+    // NearView()
+    float distance;
+    float angleView;
+    Vector3 direction;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    bool NearView() // it is true if you near interactive object
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        direction = transform.position - Camera.main.transform.position;
+        angleView = Vector3.Angle(Camera.main.transform.forward, direction);
+        if (distance < 2f) return true; // angleView < 35f && 
+        else return false;
+    }
+}
