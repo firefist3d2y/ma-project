7cc1c2bce2f6bf3caef1aa077553f20b8198e9e0
firefist3d2y <meliodasthedragon@gmx.de>
2024-01-06 23:13:37 +0100

    Added world 1 level 2 'Tree level'. Added first ramp and second layer to level 2.

--- Assets/Scripts/ObjectController/BallCameraController.cs
+++ Assets/Scripts/ObjectController/BallCameraController.cs
@@ -36,7 +36,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = (transform.position - player.transform.position)/2;
     }
 
     // Update is called once per frame
