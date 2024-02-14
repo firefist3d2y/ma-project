ea3951d091d0f03a43aae1b27aa08699cd7e4830
trompete11 <lucas.hoettler@gmail.com>
2024-02-14 23:48:26 +0100

    created Ramp
    created a ramp prefab with logic for levers

--- Assets/Scripts/ObjectController/World2/RampController.cs
+++ Assets/Scripts/ObjectController/World2/RampController.cs
@@ -9,13 +9,11 @@ public class RampController : OpenableObject
     public float OpenSpeed = 3f;
 
 
-    public Rigidbody rbRamp;
-
     //eigenes
     [Tooltip("Distance wich the Ramp should move")]
     public float moveDist = 0f;
 
-    float targetDist = 0;
+    private float targetDist = 0f;
 
     override
     protected void openObject()
@@ -29,28 +27,18 @@ public class RampController : OpenableObject
         targetDist = -moveDist;
     }
 
-    void Start()
-    {
-        rbRamp = GetComponent<Rigidbody>();
-    }
-    /*
+    
     private void FixedUpdate()
     {
-        if (currentLim < targetLim - .5f)
+        if (targetDist < -.05f)
         {
-            Debug.Log("targ gross " + targetLim + " curr: " + currentLim);
-            currentLim += .5f * OpenSpeed;
+            targetDist += .005f * OpenSpeed;
+            transform.position = transform.position + new Vector3(0, -.005f * OpenSpeed, 0);
         }
-        else if (currentLim > targetLim + .5f)
+        else if (targetDist > .05f)
         {
-            Debug.Log("targ kleiner " + targetLim + " curr: " + currentLim);
-
-            currentLim -= .5f * OpenSpeed;
+            targetDist -= .005f * OpenSpeed;
+            transform.position = transform.position + new Vector3(0, .005f * OpenSpeed, 0);
         }
-
-        // using values to door object
-        hingeLim.max = currentLim + .5f;
-        hingeLim.min = currentLim - .5f;
-        hinge.limits = hingeLim;
-    }*/
+    }
 }
