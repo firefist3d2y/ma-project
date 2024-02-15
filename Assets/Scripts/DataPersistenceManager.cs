f3809a975e3ca1d43dbfc5994c8fc0328bfed283
trompete11 <lucas.hoettler@gmail.com>
2024-02-15 16:01:09 +0100

    create DataPeritance
    created DataPresistanceManager and GameData to save level results

---
+++ Assets/Scripts/DataPersistenceManager.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class DataPersistenceManager
+{
+    private GameData gameData;
+    public string savePath;
+
+    private static DataPersistenceManager Instance;
+
+    public static DataPersistenceManager getInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = new DataPersistenceManager();
+        }
+        
+        return Instance;
+    }
+
+    public void SaveLevel(int world, int level, int score)
+    {
+        gameData.saveLevel(world, level, score);
+        SaveData();
+    }
+
+    private void SaveData()
+    {
+        try
+        {
+            // create the directory the file will be written to if it doesn't already exist
+            Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+
+            // serialize the C# game data object into Json
+            string dataToStore = JsonUtility.ToJson(gameData, true);
+
+            // write the serialized data to the file
+            using FileStream stream = new FileStream(savePath, FileMode.Create);
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.Write(dataToStore);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error occured when trying to save data to file: " + savePath + "\n" + e);
+        }
+    }
+
+    public void LoadData()
+    {
+        if (File.Exists(savePath))
+        {
+            try
+            {
+                // load the serialized data from the file
+                string dataToLoad = "";
+                using (FileStream stream = new FileStream(savePath, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+                // deserialize the data from Json back into the C# object
+                gameData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to load file at path: "
+                    + savePath + " and backup did not work.\n" + e);
+                gameData = new GameData();
+            }
+        }
+        else
+        {
+            gameData = new GameData();
+        }
+    }
+}
+
